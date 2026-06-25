95eb66547fe5bcae4a9e2b8cab44da837f394cf0
Norbey Montes Ramirez <mitchel.montes3523@gmail.com>
2026-06-25 15:59:24 -0500

    Agregando carpetas de clases y public

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+class Persona
+{
+    public function saludar()
+    {
+        echo "hola este es el primer ejercicio con programacion orientada a objetos";
+    }
+}
+
+class Calculador
+{
+}
+
+$persona = new Persona();
+$persona->saludar();
+
+?>
